420ee875082b598878e80bec50ce68479041e70d
bulharualina <bulgarualina3@gmail.com>
2024-11-03 23:16:27 +0200

    Build using cmake

---
+++ build/CMakeFiles/doggy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doggy.dir/src/main.cpp.o"
+  "CMakeFiles/doggy.dir/src/main.cpp.o.d"
+  "doggy"
+  "doggy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/doggy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/doggy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for doggy.
+# This may be replaced when dependencies are built.
